244a2dd4c07f3e9eb4e6ae0e123b58ffd485e59d
Ismail-tony <ibabalola21@outlook.com>
2024-01-14 18:54:43 +0000

    adding quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,12 +11,31 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score
 */
+/*===================================================*\
 
-const highScores = document.getElementById('#highscores');
-const clearScores = document.getElementById('#clear');
-const timerCountdown = document.getElementById('#time');
-const startScreen = document.getElementById('#start-screen');
-const startButton = document.getElementById('#start');
-const questionContainer = document.getElementById('#questions');
-const questionTitle = document.getElementById('#question-title');
-const choicesArray = document.getElementById('#choices');
+    HTML Elements for DOM manipulation
+
+\*===================================================*/
+document.addEventListener('DOMContentLoaded', function() {
+    const timerCountdown = document.getElementById('#time');
+    const startScreen = document.getElementById('#start-screen');
+    const startButton = document.getElementById('#start');
+    const questionContainer = document.getElementById('#questions');
+    const questionTitle = document.getElementById('#question-title');
+    const choicesArray = document.getElementById('#choices');
+    const endScreen = document.getElementById('#end-screen');
+    const finalScore = document.getElementById('#final-score');
+    const initialsInput = document.getElementById('#initials');
+    const submitButton = document.getElementById('#submit');
+    const feedbackContainer = document.getElementById('#feedback');
+})
+
+
+/*===================================================*\
+
+    Start Button initiate timer and present Questions
+
+\*===================================================*/
+function name(params) {
+    
+}
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,21 +1,22 @@
 const questions = [
     {
-        Question: "",
-        choices: ['a', 'b', 'c', 'd'],
+        Question: "What is the Sky father in Yoruba Pantheon called?",
+        choices: ['Sango', 'Yemoja', 'Obatala', 'Olodumare'],
+        correctChoice: 3
+    },
+    {
+        Question: "What Yoruba Pantheon flew to the sun to ask for the Sky father's help",
+        choices: ['Yemoja', 'Oshun', 'Esu', 'Alafin'],
+        correctChoice: [1]
+        
+    },
+    {
+        Question: "What Pantheon was tasked with creation of humans?",
+        choices: ['Obatala', 'Oduduwa', 'Olofin', 'Oranmiyan'],
         correctChoice: 'a'
     },
     {
-        Question: "",
-        choices: ['a', 'b', 'c', 'd'],
-        correctChoice: 'a'
-    },
-    {
-        Question: "",
-        choices: ['a', 'b', 'c', 'd'],
-        correctChoice: 'a'
-    },
-    {
-        Question: "",
+        Question: "What goddess rule over the Ocean?",
         choices: ['a', 'b', 'c', 'd'],
         correctChoice: 'a'
     },
